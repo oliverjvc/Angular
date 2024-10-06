abe81cc21e8b74cf3548516b1124a8f8359e409f
OliverJ <100935887+oliverjvc@users.noreply.github.com>
2024-10-07 00:16:34 +0200

    Update navbar component, remove search bar

--- homes-app/src/app/navbar/navbar.component.ts
+++ homes-app/src/app/navbar/navbar.component.ts
@@ -45,13 +45,6 @@ import { ChildDateComponent } from '../child-date.component';
           </ul>
           <span>Welcome to the site! Today is:</span>
           <app-child-date [currentDate]="currentDate"></app-child-date> <!-- Passing currentDate to child -->
-          <!-- Uncomment if you want a search bar -->
-          <!-- 
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-          -->
         </div>
       </div>
     </nav>
